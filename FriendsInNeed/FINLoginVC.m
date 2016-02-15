ff0ead0b4a8c48e401ff5c918aeadc016e4fb0fb
BurningAXE <milen@milens-macbook-pro.local>
2016-02-15 12:49:04 +0200

    Registration screen rotating properly.

--- FriendsInNeed/FINLoginVC.m
+++ FriendsInNeed/FINLoginVC.m
@@ -27,8 +27,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    
-    [_containerScrollView addSubview:_registrationView];
 }
 
 - (void)viewWillAppear:(BOOL)animated

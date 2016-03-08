e5a7ef6026048b0aeded7a38690446be55b8bd31
BurningAXE <milen@milens-macbook-pro.local>
2016-03-08 02:03:37 +0200

    Started to implement signal details screen.

---
+++ FriendsInNeed/FINSignalDetailsVC.h
@@ -0,0 +1,16 @@
+//
+//  FINSignalDetailsVC.h
+//  FriendsInNeed
+//
+//  Created by Milen on 08/03/16.
+//  Copyright © 2016 г. Milen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Backendless.h"
+
+@interface FINSignalDetailsVC : UIViewController
+
+- (FINSignalDetailsVC *)initWithGeoPoint:(GeoPoint *)geoPoint;
+
+@end

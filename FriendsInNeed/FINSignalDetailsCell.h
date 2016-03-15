33d85bf81cb13fad018a232d3460a637c348f299
BurningAXE <milen@milens-macbook-pro.local>
2016-03-15 19:59:13 +0200

    Signal details implementation step 1.

---
+++ FriendsInNeed/FINSignalDetailsCell.h
@@ -0,0 +1,13 @@
+//
+//  SignalDetailsCell.h
+//  FriendsInNeed
+//
+//  Created by Milen on 14/03/16.
+//  Copyright © 2016 г. Milen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FINSignalDetailsCell : UITableViewCell
+
+@end

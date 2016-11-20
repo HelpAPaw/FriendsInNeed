5dd3042db2304a063f4ff831cba36593775ee8f4
BurningAXE <milen@milens-macbook-pro.local>
2016-11-20 08:53:51 +0200

    Menu items added.

--- FriendsInNeed/FINMenuVC.m
+++ FriendsInNeed/FINMenuVC.m
@@ -7,31 +7,110 @@
 //
 
 #import "FINMenuVC.h"
+#import "FINLoginVC.h"
+#import <ViewDeck/ViewDeck.h>
 
-@interface FINMenuVC ()
+#define kMenuCell @"MenuCell"
+
+@interface FINMenuVC () <UITableViewDataSource, UITableViewDelegate>
+
+enum
+{
+    kLogin,
+    kSettings,
+    kFAQ,
+    kFeedback,
+    kAbout,
+    kMenuItemsCount
+};
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation FINMenuVC
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kMenuCell];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - UITableViewDataSource
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
 }
-*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return kMenuItemsCount;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kMenuCell];
+    
+    NSString *title;
+    switch (indexPath.row)
+    {
+        case kLogin:
+            title = @"Login";
+            break;
+        case kSettings:
+            title = @"Settings";
+            break;
+        case kFAQ:
+            title = @"FAQ";
+            break;
+        case kFeedback:
+            title = @"Feedback";
+            break;
+        case kAbout:
+            title = @"About";
+            break;
+            
+        default:
+            break;
+    }
+    cell.textLabel.text = title;
+    
+    cell.backgroundColor = [UIColor clearColor];
+    cell.textLabel.textColor = [UIColor whiteColor];
+    cell.indentationLevel = 2;
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    switch (indexPath.row)
+    {
+        case kLogin:
+        {
+            [self.viewDeckController closeSide:YES];
+            
+            FINLoginVC *loginVC = [[FINLoginVC alloc] init];
+            loginVC.modalPresentationStyle = UIModalPresentationOverCurrentContext;
+            [self presentViewController:loginVC animated:YES completion:^{}];
+            break;
+        }
+            
+        default:
+            break;
+    }
+}
 
 @end

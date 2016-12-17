8f62cea93cc12a4953da562920678b589c5a3c88
BurningAXE <milen@milens-macbook-pro.local>
2016-12-17 16:21:00 +0200

    About view controller created.

---
+++ FriendsInNeed/FINAboutVC.h
@@ -0,0 +1,13 @@
+//
+//  FINAboutVC.h
+//  FriendsInNeed
+//
+//  Created by Milen on 17/12/16.
+//  Copyright © 2016 Milen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FINAboutVC : UIViewController
+
+@end

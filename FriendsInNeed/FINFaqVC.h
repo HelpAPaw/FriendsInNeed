6650877836709e5e5d3e7aa9290ae9bab9450fe6
BurningAXE <milen@milens-macbook-pro.local>
2016-12-17 17:50:37 +0200

    FAQ view controller added.

---
+++ FriendsInNeed/FINFaqVC.h
@@ -0,0 +1,13 @@
+//
+//  FINFaqVC.h
+//  FriendsInNeed
+//
+//  Created by Milen on 17/12/16.
+//  Copyright © 2016 Milen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FINFaqVC : UIViewController
+
+@end

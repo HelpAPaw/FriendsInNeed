24f04853b1c0d073d97655c4cf7cead3c1e0c2a3
BurningAXE <milen.danchev.marinov@gmail.com>
2018-10-28 18:46:07 +0200

    236 Fix auto layout issue

--- FriendsInNeed/FINSettingsSliderCell.swift
+++ FriendsInNeed/FINSettingsSliderCell.swift
@@ -16,11 +16,6 @@ class FINSettingsSliderCell: UITableViewCell {
     public var valueChangedCallback: ((Int) -> Void)?
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var slider: UISlider!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)

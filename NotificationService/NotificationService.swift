5a060bff1e19d01b5089006b6be5300c7d8935b1
BurningAXE <milen.danchev.marinov@gmail.com>
2019-02-24 22:19:09 +0200

    416 Setup push notifications with Backendless

---
+++ NotificationService/NotificationService.swift
@@ -0,0 +1,27 @@
+//
+//  NotificationService.swift
+//  NotificationService
+//
+//  Created by Milen on 24/02/19.
+//  Copyright © 2019 Milen. All rights reserved.
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+    
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+    
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        BackendlessPushHelper.sharedInstance().processMutableContent(request, withContentHandler: contentHandler)
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+}

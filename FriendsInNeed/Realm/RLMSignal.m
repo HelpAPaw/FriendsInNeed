d45c75febcdd98d7f150fa25af8ea1506eddf280
BurningAXE <milen.danchev.marinov@gmail.com>
2019-06-05 18:29:21 +0300

    455 Add Realm to project. Save notification shown status.

---
+++ FriendsInNeed/Realm/RLMSignal.m
@@ -0,0 +1,13 @@
+//
+//  RLMSignal.m
+//  Help A Paw
+//
+//  Created by Milen on 04/06/19.
+//  Copyright © 2019 Milen. All rights reserved.
+//
+
+#import "RLMSignal.h"
+
+@implementation RLMSignal
+
+@end

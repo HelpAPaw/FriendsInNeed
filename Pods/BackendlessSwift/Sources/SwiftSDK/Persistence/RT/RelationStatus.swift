9f2b06721194a7582c35c4bc67ffc9788ccf4ae8
Milen Marinov <milen.danchev.marinov@gmail.com>
2020-12-06 23:12:33 +0200

    526 Migrate to Firebase Crashlytics; Update all PODs #2

---
+++ Pods/BackendlessSwift/Sources/SwiftSDK/Persistence/RT/RelationStatus.swift
@@ -0,0 +1,29 @@
+//
+//  RelationStatus.swift
+//
+/*
+ * *********************************************************************************************************************
+ *
+ *  BACKENDLESS.COM CONFIDENTIAL
+ *
+ *  ********************************************************************************************************************
+ *
+ *  Copyright 2020 BACKENDLESS.COM. All Rights Reserved.
+ *
+ *  NOTICE: All information contained herein is, and remains the property of Backendless.com and its suppliers,
+ *  if any. The intellectual and technical concepts contained herein are proprietary to Backendless.com and its
+ *  suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret
+ *  or copyright law. Dissemination of this information or reproduction of this material is strictly forbidden
+ *  unless prior written permission is obtained from Backendless.com.
+ *
+ *  ********************************************************************************************************************
+ */
+
+import Foundation
+
+@objcMembers public class RelationStatus: NSObject {
+    public var parentObjectId: String?
+    public var isConditional = false
+    public var whereClause: String?
+    public var children: [String]?
+}

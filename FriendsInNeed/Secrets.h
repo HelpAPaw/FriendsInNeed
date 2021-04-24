329f0c2e7ea642adcfec0f320444b792c610d912
Milen Marinov <milen.danchev.marinov@gmail.com>
2021-04-24 15:52:07 +0300

    .gitignore fix

---
+++ FriendsInNeed/Secrets.h
@@ -0,0 +1,16 @@
+//
+//  Secrets.h
+//  FriendsInNeed
+//
+//  Created by Milen Marinov on 24.04.21.
+//  Copyright © 2021 Milen. All rights reserved.
+//
+
+#ifndef Secrets_h
+#define Secrets_h
+
+#define BCKNDLSS_APP_ID         @"BDCD56B9-351A-E067-FFA4-9EA9CF2F4000"
+#define BCKNDLSS_IOS_API_KEY    @"9F8B017B-2890-A887-FFD5-63D6A5302100"
+#define BCKNDLSS_REST_API_KEY   @"473D7C4A-3FBC-69F4-FFFF-1635F07E4300"
+
+#endif /* Secrets_h */
